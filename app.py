01ec85396b018e8d4620b0b6ecee63ed099e7aee
Sanjay Kumar <74352452+1242sanjay@users.noreply.github.com>
2022-07-07 13:54:27 +0000

    added HTML and css

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello world"
+  return render_template("home.html")
 
-@app.route("/hello")
-def hello():
-  return "<h2>Hello jovian</h2>"
   
 
 
